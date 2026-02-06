d97c3d1fb32fcca16e85ba3eb0856ddd0ce75af4
SS11146111 <sahasangita59@gmail.com>
2026-02-06 14:21:29 +0530

    Arrays.fill method

---
+++ problems/src/ArraysFill.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArraysFill {
+
+    public static void main(String[] args) {
+
+        //By default array is initialised with 0, to override it with another value use fill
+        int[] arr = new int[10];
+        Arrays.fill(arr,5);
+        System.out.println(Arrays.toString(arr));
+    }
+}
