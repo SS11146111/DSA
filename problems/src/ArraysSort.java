22f07884cf0cd7b43f77dbddc94a0c3341c1085c
SS11146111 <sahasangita59@gmail.com>
2026-02-06 13:48:29 +0530

    Arrays.sort method

---
+++ problems/src/ArraysSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class ArraysSort {
+
+    public static void main(String[] args) {
+
+        //sorting array
+
+        int[] arr = new int[] {10,5,4,3,12,8};
+
+        // variation 1, sorting entire array in ascending by default
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        //variation 2, sorting subarray or part of the main array, end index is exclusive
+        Arrays.sort(arr,3, arr.length);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
