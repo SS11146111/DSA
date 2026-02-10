78900b32aa9949169486fe7f5e491f62929876df
SS11146111 <sahasangita59@gmail.com>
2026-02-10 13:31:05 +0530

    Bubble sort

---
+++ problems/src/Sorting/Bubble.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+public class Bubble {
+    public static void main(String[] args) {
+
+        int[] arr = {20,10,40,50,25,15};
+        int temp;
+        int n = arr.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-1-i;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1] = temp;
+
+                }
+            }
+        }
+
+        for(int k : arr)
+        {
+            System.out.print(k+ " ");
+        }
+
+    }
+}
