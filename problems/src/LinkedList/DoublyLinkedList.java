b0a8ae22ee305b610955a61a128c16465611663e
SS11146111 <sahasangita59@gmail.com>
2026-02-13 19:49:05 +0530

    updated DoublyLinkedList class

--- problems/src/LinkedList/DoublyLinkedList.java
+++ problems/src/LinkedList/DoublyLinkedList.java
@@ -28,26 +28,33 @@ public class DoublyLinkedList {
 
     public static void printList()
     {
-        DNode current = head;
-
-        System.out.println("Forward traversal");
-        while(current.getNext()!=null)
+        if(head == null)
         {
-            System.out.print(current.getData()+"->");
-            current=current.getNext();
+            return;
         }
-        System.out.print(current.getData());
-        System.out.println();
-
-
-        System.out.println("Backward traversal");
-        while(current.getPrev()!=null)
+        else
         {
-            System.out.print(current.getData()+"->");
-            current=current.getPrev();
+            DNode current = head;
+
+            System.out.println("Forward traversal");
+            while(current.getNext()!=null)
+            {
+                System.out.print(current.getData()+"->");
+                current=current.getNext();
+            }
+            System.out.print(current.getData());
+            System.out.println();
+
+
+            System.out.println("Backward traversal");
+            while(current.getPrev()!=null)
+            {
+                System.out.print(current.getData()+"->");
+                current=current.getPrev();
+            }
+            System.out.print(current.getData());
+            System.out.println();
         }
-        System.out.print(current.getData());
-        System.out.println();
     }
 
     public static void insertionAtFront(int data)
@@ -98,7 +105,13 @@ public class DoublyLinkedList {
         {
             head = newNode;
         }
-        else
+        else if(head!=null && position == 1)
+        {
+            newNode.setNext(head);
+            head.setPrev(newNode);
+            head = newNode;
+        }
+        else if(head!=null && head.getNext()!=null && position >= 2)
         {
             for(int i=1;i<position-1;i++)
             {
@@ -109,6 +122,77 @@ public class DoublyLinkedList {
             newNode.setPrev(current);
             current.setNext(newNode);
             newNode.getNext().setPrev(newNode);
+        }
+    }
+
+    public static void deletionAtFront()
+    {
+
+        if (head != null && head.getNext() == null)
+        {
+            head = null;
+        }
+        else if(head != null && head.getNext()!=null)
+        {
+
+            head=head.getNext();
+            head.setPrev(null);
+        }
+
+    }
+
+    public static void deletionAtEnd()
+    {
+        DNode current = head;
+
+        if(head!=null && head.getNext()==null)
+        {
+            head=null;
+        }
+        else if(head!=null && head.getNext() != null)
+        {
+            while(current.getNext()!=null)
+            {
+                current = current.getNext();
+            }
+
+            current.getPrev().setNext(null);
+            current.setPrev(null);
+        }
+    }
+
+    public static void deletionInMiddle(int position)
+    {
+        if(head == null)
+        {
+            return;
+        }
+        else if(head!=null && head.getNext()==null && position == 1)
+        {
+            head = null;
+        }
+        else if(head!=null && head.getNext()!=null && position ==1)
+        {
+            head = head.getNext();
+            head.getPrev().setNext(null);
+            head.setPrev(null);
+        }
+        else if(head!=null && head.getNext()!=null && position>=2)
+        {
+            DNode current = head;
+            for(int i=1; i<position-1;i++)
+            {
+                current = current.getNext();
+            }
+            System.out.println("Deleting data : "+current.getNext().getData());
+
+            DNode temp = current.getNext();
+            current.getNext().getNext().setPrev(current);
+            current.setNext(current.getNext().getNext());
+
+            temp.setNext(null);
+            temp.setPrev(null);
+
         }
     }
 
@@ -124,6 +208,14 @@ public class DoublyLinkedList {
         printList();
         insertionInMiddle(3,2);
         insertionInMiddle(36,7);
+        printList();
+        deletionAtFront();
+        printList();
+        deletionAtEnd();
+        printList();
+        deletionInMiddle(2);
+        printList();
+        deletionInMiddle(5);
         printList();
     }
 
