bcf644061ea88646c97f8b778d402fb970b2ef30
SS11146111 <sahasangita59@gmail.com>
2026-02-13 16:21:41 +0530

    implemented printing linkedlist

--- problems/src/LinkedList/List.java
+++ problems/src/LinkedList/List.java
@@ -2,20 +2,42 @@ package LinkedList;
 
 public class List {
 
+    private static Node head = null;
+    private static Node ten = new Node(10);
+    private static Node twenty = new Node (20);
+    private static Node thirty = new Node (30);
+    private static Node forty = new Node (40);
+    private static Node fifty = new Node (50);
 
-    public static void main(String[] args) {
-
-        Node ten = new Node(10);
-        Node twenty = new Node (20);
-        Node thirty = new Node (30);
-        Node forty = new Node (40);
-        Node fifty = new Node (50);
-
-        Node head = ten;
+    public static Node createList()
+    {
+        head = ten;
         ten.setNext(twenty);
         twenty.setNext(thirty);
         thirty.setNext(forty);
         forty.setNext(fifty);
         fifty.setNext(null);
+        return head;
+    }
+
+    public static void printList(Node head)
+    {
+        Node current = head;
+        while(current.getNext()!=null)
+        {
+            System.out.print(current.getData()+"->");
+            current = current.getNext();
+        }
+
+        System.out.print(current.getData());
+
+    }
+    public static void main(String[] args) {
+
+        Node head = createList();
+        printList(head);
+
+
+
     }
 }
