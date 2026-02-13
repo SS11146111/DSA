44685971bedb0d194acfd4b0df6382b87fa3d569
SS11146111 <sahasangita59@gmail.com>
2026-02-13 18:18:26 +0530

    creation and printing of doubly linked list

--- problems/src/LinkedList/DNode.java
+++ problems/src/LinkedList/DNode.java
@@ -2,9 +2,9 @@ package LinkedList;
 
 public class DNode {
 
-    int data;
-    DNode prev;
-    DNode next;
+    private int data;
+    private DNode prev;
+    private DNode next;
 
     DNode(int data) {
         this.data = data;
--- problems/src/LinkedList/DoublyLinkedList.java
+++ problems/src/LinkedList/DoublyLinkedList.java
@@ -2,5 +2,61 @@ package LinkedList;
 
 public class DoublyLinkedList {
 
+    private static DNode head = null;
+    private static DNode first = new DNode(10);
+    private static DNode second = new DNode(20);
+    private static DNode third = new DNode(30);
+    private static DNode fourth = new DNode(40);
+    private static DNode fifth = new DNode(50);
+
+    public static void createDoublyLinkedList()
+    {
+        head = first;
+        first.setNext(second);
+
+        second.setPrev(first);
+        second.setNext(third);
+
+        third.setPrev(second);
+        third.setNext(fourth);
+
+        fourth.setPrev(third);
+        fourth.setNext(fifth);
+
+        fifth.setPrev(fourth);
+    }
+
+    public static void printList()
+    {
+        DNode current = head;
+
+        System.out.println("Forward traversal");
+        while(current.getNext()!=null)
+        {
+            System.out.print(current.getData()+"->");
+            current=current.getNext();
+        }
+        System.out.print(current.getData());
+        System.out.println();
+
+
+        System.out.println("Backward traversal");
+        while(current.getPrev()!=null)
+        {
+            System.out.print(current.getData()+"->");
+            current=current.getPrev();
+        }
+        System.out.print(current.getData());
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        createDoublyLinkedList();
+        printList();
+
+
+    }
+
 
 }
