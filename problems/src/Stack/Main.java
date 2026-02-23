796ef7e6b3cb33349435b5d68d6f391e8b265972
SS11146111 <sahasangita59@gmail.com>
2026-02-23 15:42:53 +0530

    stack push

---
+++ problems/src/Stack/Main.java
@@ -0,0 +1,20 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(30);
+        stack.push(5);
+        stack.push(15);
+        stack.push(6);
+
+
+    }
+
+
+}
